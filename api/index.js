d7505e9f7f10716b673a6eca17b197e9fc952073
manish-jandu <manishjandu1@gmail.com>
2023-07-08 16:08:03 +0530

    posting and getting post setup

--- api/index.js
+++ api/index.js
@@ -4,18 +4,24 @@ const cors = require("cors");
 const { connectMongoose } = require("./connection/dbConnection");
 const port = 4000;
 const User = require("./models/User");
+const Post = require("./models/Post")
 const bcrypt = require("bcryptjs");
 const salt = bcrypt.genSaltSync(10);
 require("dotenv").config();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 
 connectMongoose();
+
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
@@ -72,7 +78,37 @@ app.post('/logout', (req,res) => {
   res.cookie('token', '').json('ok');
 });
 
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
 
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {title,summary,content} = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover:newPath,
+      author:info.id,
+    });
+    res.json(postDoc);
+  });
+
+});
+
+app.get('/post', async (req,res) => {
+  res.json(
+    await Post.find()
+      .populate('author', ['username'])
+      .sort({createdAt: -1})
+      .limit(20)
+  );
+});
 
 
 app.listen(port, () => {
